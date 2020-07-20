d969f98bfa61a76d80465deee58ae51772e32753
donimuvukor <donimuvukor@gmail.com>
2020-07-20 10:26:06 +0100

    Created README and countdown clock
    Implemented repository README and coiuntdown clock

---
+++ README.md
@@ -0,0 +1,13 @@
+# AutoMate 
+
+An app that provides you with perfect replies to your partner during a conversation. 
+ 
+Using state of the art voice pattern recognition algorithms, AutoMate senses your partner's mood, taking note of their tone and the actual words spoken, and gives you the perfect, situation appropriate comeback. 
+ 
+It can also records conversations with your partner so you can listen to them again and go over parts you may have missed.
+
+## Designer: @Atinuke
+## Front End Developer: @Doni
+## E-mail: donimuvukor@gmail.com
+## Link: https://automate-hngi7.herokuapp.com/index.html
+## Design: https://www.figma.com/file/MQYjcpiSfS8QxU2umEVeMz/Baker's-Portfolio?node-id=206%3A2
--- js/app.js
+++ js/app.js
@@ -20,3 +20,39 @@ new Glide('.glide',
 
 //Initialise animate on scroll effects
 AOS.init();
+
+
+//Countdown Clock
+const deadline = 'July 31, 2020';
+const clockItems = ['days', 'hours', 'minutes', 'seconds'];
+
+function getTimeRemaining (endtime) {
+    const total = Date.parse(endtime) - Date.parse( new Date() );
+    const seconds = Math.floor( (total/1000) % 60 );
+    const minutes = Math.floor( (total/1000/60) % 60 );
+    const hours = Math.floor( (total/(1000*60*60)) % 24 );
+    const days = Math.floor( total/(1000*60*60*24) );
+    return {
+        total,
+        days,
+        hours,
+        minutes,
+        seconds
+    };
+}
+
+function initializeClock (id, endtime) {
+    const clockItem = document.getElementById(id);
+    const timeinterval = setInterval( () => {
+        const timeRemaining = getTimeRemaining(endtime);
+        clockItem.innerHTML = timeRemaining[clockItem.id];
+        if (timeRemaining.total <= 0) {
+            clearInterval(timeinterval);
+        }
+    }, 1000);
+}
+
+//Start all items of countdown clock
+for (let i = 0; i < clockItems.length; i++) {
+    initializeClock(clockItems[i], deadline);
+}
